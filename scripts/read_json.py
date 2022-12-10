039e6cd6290855ab94540fe516df6ce4c12e60fd
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-10 12:49:29 -0600

    f

---
+++ scripts/read_json.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import sys
+# Agrega la ruta de la carpeta que contiene el archivo JSON a la ruta de búsqueda de Python
+# Primero obtiene la ruta absoluta del archivo actual
+ruta_actual = os.path.dirname(os.path.abspath('read_json.py'))
+# Luego, construye la ruta completa de la carpeta que contiene el archivo JSON
+ruta_carpeta = os.path.join(ruta_actual, '/home/dianamunnoz/Syntactical_similarities_between_texts/data')
+# Finalmente, agrega la ruta de la carpeta a la ruta de búsqueda de Python
+sys.path.append(ruta_carpeta)
+
+df = pd.read_json('train-v2.0.json')
+print("Muestra los primeros 10 datos")
+df.head(10)
