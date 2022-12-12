3ecfbe8c8956b7e3589aa2c6e5227f2a63e6b5fc
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-11 22:00:23 -0600

    unos pedillos

--- main.sh
+++ main.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
 WORKING_DIR=$(pwd)
-WORKING_DIR=$WORKING_DIR/data
 
-if [[ -f $WORKING_DIR/data.json ]]
+if [[ -f $WORKING_DIR/scripts/data.json ]]
 then
-    rm $WORKING_DIR/data.json
+    rm $WORKING_DIR/scripts/data.json
 fi
 
-cat $WORKING_DIR/train-v2.0.json | jq '[. | .data | .[] | .t=(.title) | .paragraphs | .[] | .questions=([.qas | .[] | .question]) | .ans=([.qas | .[] | .. | .text?]) | {context: .context, questions:.questions, ans:.ans}]' > $WORKING_DIR/data.json
+cat $WORKING_DIR/data/train-v2.0.json | jq '[. | .data | .[] | .t=(.title) | .paragraphs | .[] | .questions=([.qas | .[] | .question]) | .ans=([.qas | .[] | .. | .text?]) | {context: .context, questions:.questions, ans:.ans}]' > $WORKING_DIR/scripts/data.json
--- scripts/read_json.py
+++ scripts/read_json.py
@@ -5,12 +5,12 @@ import os
 import sys
 # Agrega la ruta de la carpeta que contiene el archivo JSON a la ruta de búsqueda de Python
 # Primero obtiene la ruta absoluta del archivo actual
-ruta_actual = os.path.dirname(os.path.abspath('read_json.py'))
+#ruta_actual = os.path.dirname(os.path.abspath('read_json.py'))
 # Luego, construye la ruta completa de la carpeta que contiene el archivo JSON
-ruta_carpeta = os.path.join(ruta_actual, '../Syntactical_similarities_between_texts/data')
+#ruta_carpeta = os.path.join(ruta_actual, '/home/dianamunnoz/Syntactical_similarities_between_texts/data')
 # Finalmente, agrega la ruta de la carpeta a la ruta de búsqueda de Python
-print(ruta_carpeta)
-sys.path.append(ruta_carpeta)
+#print(ruta_carpeta)
+#sys.path.append(ruta_carpeta)
 df = pd.read_json('data.json')
 print("Muestra los primeros 10 datos")
 df.head(10)
