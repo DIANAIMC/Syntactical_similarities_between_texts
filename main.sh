7aed9853ae2fa94ee015d648b194ed4417994675
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-11 21:30:05 -0600

    cambiecitos

--- main.sh
+++ main.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-Hola soy adaya
+WORKING_DIR=$(pwd)
+WORKING_DIR=$WORKING_DIR/data
+
+if [[ -f $WORKING_DIR/data.json ]]
+then
+    rm $WORKING_DIR/data.json
+fi
+
+cat $WORKING_DIR/train-v2.0.json | jq '[. | .data | .[] | .t=(.title) | .paragraphs | .[] | .questions=([.qas | .[] | .question]) | .ans=([.qas | .[] | .. | .text?]) | {context: .context, questions:.questions, ans:.ans}]' > $WORKING_DIR/data.json
