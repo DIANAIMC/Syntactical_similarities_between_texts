25bce91066049129809be45c8b9f04edf2797b3f
Adaya Isui Escobar Fortis <adayaisui@MacBook-Air-de-Adaya.local>
2022-12-11 21:36:55 -0600

    por favor funciona

--- scripts/read_json.py
+++ scripts/read_json.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import pandas as pd
 import os
 import sys
@@ -5,7 +7,7 @@ import sys
 # Primero obtiene la ruta absoluta del archivo actual
 ruta_actual = os.path.dirname(os.path.abspath('read_json.py'))
 # Luego, construye la ruta completa de la carpeta que contiene el archivo JSON
-ruta_carpeta = os.path.join(ruta_actual, '/home/dianamunnoz/Syntactical_similarities_between_texts/data')
+ruta_carpeta = os.path.join(ruta_actual, '../Syntactical_similarities_between_texts/data')
 # Finalmente, agrega la ruta de la carpeta a la ruta de búsqueda de Python
 print(ruta_carpeta)
 sys.path.append(ruta_carpeta)
