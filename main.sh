98db13a09e596d6294220df682e59cbcdcd6ef4d
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-11 21:14:38 -0600

    Prueba para adaya

--- main.sh
+++ main.sh
@@ -1,1 +1,3 @@
 #! /bin/bash
+
+Hola soy adaya
--- scripts/read_json.py
+++ scripts/read_json.py
@@ -7,8 +7,9 @@ ruta_actual = os.path.dirname(os.path.abspath('read_json.py'))
 # Luego, construye la ruta completa de la carpeta que contiene el archivo JSON
 ruta_carpeta = os.path.join(ruta_actual, '/home/dianamunnoz/Syntactical_similarities_between_texts/data')
 # Finalmente, agrega la ruta de la carpeta a la ruta de búsqueda de Python
+print(ruta_carpeta)
 sys.path.append(ruta_carpeta)
-
-df = pd.read_json('train-v2.0.json')
+df = pd.read_json('data.json')
 print("Muestra los primeros 10 datos")
 df.head(10)
+
