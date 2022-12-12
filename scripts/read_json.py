98d851527e02e770a49f6f92b093af2f8a161dcb
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-11 23:27:21 -0600

    lol

--- scripts/read_json.py
+++ scripts/read_json.py
@@ -6,31 +6,24 @@ import sys
 
 df = pd.read_json('data.json')
 
+print("	> Todo a lowercase")
 
-print("Todo a lowercase")
 df['context'] = df['context'].apply(str.lower)
 df['questions'] = df['questions'].apply(lambda x: [e.lower() if isinstance(e, str) else e for e in x])
 df['ans'] = df['ans'].apply(lambda x: [e.lower() if isinstance(e, str) else e for e in x])
-print("Muestra los primeros 10 datos")
-print(df.head(10))
 
-print("Eliminar stopwords")
-# Read the words from the text file into a list
+print("	> Elimina stopwords")
+
+print(df['context'])
 with open('stopwords.txt', 'r') as f:
     words = f.read().split()
 
-# Define a custom function that will remove the words from the text file
-def remove_words(text):
-    # Split the text into a list of words
-    text_words = text.split()
-    
-    # Remove any words that are in the list of words from the text file
-    text_words = [w for w in text_words if w not in words]
-    
-    # Reconstruct the text from the remaining words
-    text = ' '.join(text_words)
-    
-    return text
+def remove_list_of_words(column_to_modify, stopwords):
+    for word in stopwords:
+        column_to_modify = column_to_modify.replace(word, "")
+    return column_to_modify
+remove_list_of_words(df['context'], words)
 
-# Apply the custom function to each element of the 'column1' column
-df['context'] = df['context'].apply(remove_words)
+df['context'] = remove_list_of_words(df['context'],words)
+
+print(df['context'])
