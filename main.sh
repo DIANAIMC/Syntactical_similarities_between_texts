5d2ff3cfc4d664a52c8d2dc3e5f7ce7951ab60c6
DIANAIMC <dianamunnoz@LAPTOP-DLR8O4OC>
2022-12-13 07:14:25 -0600

    intento2

--- main.sh
+++ main.sh
@@ -9,8 +9,7 @@ fi
 
 echo -e '\n--------------------- LIMPIEZA DE JSON ---------------------'
 echo '	>Estructura a json'
-cat $WORKING_DIR/data/train-v2.0.json | jq '[. | .data | .[] | .t=(.title) | .paragraphs | .[] | .questions=([.qas | .[] | .question]) | .ans=([.qas | .[] | .. | .text?]) | {context: .context, questions:.questions, ans:.ans}]' > $WORKING_DIR/scripts/data.json
+cat $WORKING_DIR/data/dev-v2.0.json | jq '[. | .data | .[] | .t=(.title) | .paragraphs | .[] | .questions=([.qas | .[] | .question]) | .ans=([.qas | .[] | .. | .text?]) | {context: .context, questions:.questions, ans:.ans}]' > $WORKING_DIR/scripts/data.json
 
-#Tenemos ver qué onda con las direcciones en python
 
-python ./scripts/clean_json.py
+#python ./scripts/clean_json.py
